6d2be711357422d9e5f9e1d7787f13caed994554
michalskrzynski <michal.skrzynski@gmail.com>
2022-08-31 19:27:19 +0200

    resize pane, modal input

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const screenWidth = 960;
-let boxArray = [];
 
 //paneId can come with or no # sign
 const drawPane = function(paneId, xBoxes) {
@@ -10,14 +9,33 @@ const drawPane = function(paneId, xBoxes) {
   const pane = document.querySelector( '#' + paneId.replace('#')); 
   if( !pane ) { console.error('NO PANE DIV!'); return; }
 
+  
+  //removing all existing boxes wrapped by pane-hook
+  //pane-hook is created for easiness of boxes removal
+  let paneHook = document.querySelector('div#pane-hook');
+  if( paneHook ) pane.removeChild( paneHook );
 
+  //creating new pane-hook
+  paneHook = document.createElement('div');
+  paneHook.setAttribute( 'id', 'pane-hook');
+  pane.appendChild( paneHook );
+
+  
+  
+  //creating boxes
   const boxSize = Math.floor( screenWidth / xBoxes );
+  let boxArray = [];
   for( let i=0; i< Math.pow(xBoxes, 2); i++ ) {
     boxArray.push( createBoxNode(boxSize) );
   }
 
-  pane.append(...boxArray);
+  //and adding to DOM under pane-hook
+  paneHook.append(...boxArray);
 }
+
+
+
+
 
 const createBoxNode = function (boxSize) {
   const box = document.createElement('div');
@@ -32,7 +50,17 @@ const createBoxNode = function (boxSize) {
 
 
 drawPane( 'touchpane', 16);
-document.querySelector('#reset').addEventListener('click', () => {
-  let popup = document.querySelector('#popup');
-  popup.setAttribute( 'style', 'display: block') 
+document.querySelector('button#reset').addEventListener('click', () => {
+  document.querySelector('#popup').setAttribute( 'style', 'display: flex');
+});
+
+document.querySelector('button#confirm-reset').addEventListener('click', () => {
+  const sideX = document.querySelector('#sideX').value;
+  const xNumber = Number( sideX );
+
+  //drawing new pane
+  if( xNumber ) drawPane('touchpane', Math.min( 100, xNumber) );
+
+  //closing popup
+  document.querySelector('#popup').setAttribute( 'style', 'display: none');
 });
